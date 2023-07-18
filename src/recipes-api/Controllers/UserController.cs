76431363174cf0fe9980f56173ced2523cbb55de
liciniopsj <licinio.psj@gmail.com>
2023-07-18 20:57:54 -0300

    User Controller Req 06

--- src/recipes-api/Controllers/UserController.cs
+++ src/recipes-api/Controllers/UserController.cs
@@ -24,7 +24,11 @@ public class UserController : ControllerBase
     [HttpGet("{email}", Name = "GetUser")]
     public IActionResult Get(string email)
     {                
-        throw new NotImplementedException();
+        if(this._service.UserExists(email))
+        {
+            return Ok(this._service.GetUser(email));
+        }
+        return NotFound();
     }
 
     // 7 - Sua aplicação deve ter o endpoint POST /user
