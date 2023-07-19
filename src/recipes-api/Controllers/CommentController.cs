12bfbe09285e2f9fce2b4d49b2ae9398e76e7321
liciniopsj <licinio.psj@gmail.com>
2023-07-18 21:27:37 -0300

    UserController Done

--- src/recipes-api/Controllers/CommentController.cs
+++ src/recipes-api/Controllers/CommentController.cs
@@ -24,13 +24,15 @@ public class CommentController : ControllerBase
     [HttpPost]
     public IActionResult Create([FromBody]Comment comment)
     {
-        throw new NotImplementedException();
+        this._service.AddComment(comment);
+        return Created("", comment);
     }
 
     // 11 - Sua aplicação deve ter o endpoint GET /comment/:recipeName
     [HttpGet("{name}", Name = "GetComment")]
     public IActionResult Get(string name)
     {                
-        throw new NotImplementedException();                   
+        this._service.GetComments(name);  
+        return Created("", this._service.GetComments(name));                 
     }
 }
